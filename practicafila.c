45ce74697dc658ff87a4e8d93f772f1c0e2c69fe
Alan Gamboa <alangam97@gmail.com>
2019-08-31 09:49:29 -0500

    empezando con el char de la estructura

--- practicafila.c
+++ practicafila.c
@@ -101,8 +101,9 @@ Fila *fila3 = NULL;
 Fila *filaimpresion = NULL;
 Fila *ptr1;
 int inicio = 1;
+int i;
 
-srand(1);
+srand(time(NULL));
 
 
 
@@ -115,28 +116,50 @@ for (int i = 0; i<10; i++){
 }
 
 //ptr1 = bajaFila(fila1);
+printf("ANTES DE impresion");
+printf("\nFila 1: ");
+verFila(fila1);
+printf("\nFila 2: ");
+verFila(fila2);
+printf("\nFila 3: ");
+verFila(fila3);
 
 
+for(i = 0; i<45; i++){
+  if(rand()%4  == 1){
+      if(fila1 == NULL){
+        i--;
+      }else{
+    filaimpresion = altaFila(filaimpresion, fila1->dato);
+    fila1 = bajaFila(fila1);
+  }
+  }
 
 
-for(int i = 0; i<10; i++){
-  if(rand()%4 == 1){
-filaimpresion = altaFila(filaimpresion, fila1->dato);
-fila1 = bajaFila(fila1);
+if(rand()%4 == 2){
+  if(fila2 == NULL){
+    i--;
+  }
 
-}if(rand()%4 == 2){
+else{
   filaimpresion = altaFila(filaimpresion, fila2->dato);
   fila2 = bajaFila(fila2);
-}if(rand()%4 == 3){
+}
+}
 
+if(rand()%4 == 3){
+    if(fila3 == NULL){
+  i--;
+  }else{
     filaimpresion = altaFila(filaimpresion, fila3->dato);
     fila3 = bajaFila(fila3);
+  }
   }
 
 
 }
 
-
+printf("\nDespues de 30 segundos:\n");
 printf("Fila 1: ");
 verFila(fila1);
 printf("\nFila 2: ");
@@ -144,8 +167,10 @@ verFila(fila2);
 printf("\nFila 3: ");
 verFila(fila3);
 
+
 printf("\n Fila impresion: " );
 verFila(filaimpresion);
+Tamano(filaimpresion);
 
 
 

81aeff70dbcef783bdb49a5a1cf6c4dadcfde8a0
Alan Gamboa <alangam97@gmail.com>
2019-08-31 15:13:18 -0500

    no funciona :c

--- practicafila.c
+++ practicafila.c
@@ -8,24 +8,24 @@
 
 typedef struct _Nodo
 {
-	int dato;
+	int id;
 	struct _Nodo *sig;
 }Fila;
 
 
-Fila *crearNodo(int dato)
+Fila *crearNodo(int id)
 {
 	Fila *nuevo;
 	nuevo = (Fila*)malloc(sizeof(Fila));
-	nuevo -> dato=dato;
+	nuevo -> id=id;
   nuevo -> sig=NULL;
   return nuevo;
 }
 
-Fila *altaFila(Fila *inicio, int dato)
+Fila *altaFila(Fila *inicio, int id)
 {
 	Fila *nuevo, *aux;
-	nuevo = crearNodo(dato);
+	nuevo = crearNodo(id);
 	if(inicio==NULL)
 	{
 		inicio = nuevo;
@@ -72,8 +72,8 @@ void verFila(Fila *nodo)
 		{
           while(nodo!=NULL)
 		  {
-            printf("%d ",nodo->dato);
-            nodo=nodo->sig;
+            printf("%d ",nodo->id);
+            nodo = nodo->sig;
           }
         }
 }
@@ -92,6 +92,52 @@ void Tamano(Fila *cima){
     printf("Tamanio: %d\n",contador);
 }
 
+void procesoImpresion(Fila *fila1, Fila* fila2, Fila *fila3, Fila *filaimpresion)
+{
+int i;
+  for (int i = 0; i<10; i++){
+    fila1 = altaFila(fila1,1);
+    fila2 = altaFila(fila2,2);
+    fila3 = altaFila(fila3,3);
+}
+for(i = 0; i<45; i++){
+  if(rand()%4  == 1){
+      if(fila1 == NULL){
+        i--;
+      }else{
+    filaimpresion = altaFila(filaimpresion, fila1->id);
+    fila1 = bajaFila(fila1);
+  }
+  }
+
+
+if(rand()%4 == 2){
+
+  if(fila2 == NULL){
+    i--;
+  }
+
+else{
+  filaimpresion = altaFila(filaimpresion, fila2->id);
+  fila2 = bajaFila(fila2);
+}
+}
+
+if(rand()%4 == 3){
+
+    if(fila3 == NULL){
+  i--;
+  }else{
+    filaimpresion = altaFila(filaimpresion, fila3->id);
+    fila3 = bajaFila(fila3);
+  }
+  }
+
+
+}
+
+}
+
 int main(){
 
 int delay = 30;
@@ -102,62 +148,22 @@ Fila *filaimpresion = NULL;
 Fila *ptr1;
 int inicio = 1;
 int i;
+char id1[4]= "P1_F";
+char id2[4]= "P2_F";
+char id3[4]= "P3_F";
 
 srand(time(NULL));
 
 
 
 
-for (int i = 0; i<10; i++){
-  fila1 = altaFila(fila1,1);
-  fila2 = altaFila(fila2,2);
-  fila3 = altaFila(fila3,3);
-
-}
 
 //ptr1 = bajaFila(fila1);
-printf("ANTES DE impresion");
-printf("\nFila 1: ");
-verFila(fila1);
-printf("\nFila 2: ");
-verFila(fila2);
-printf("\nFila 3: ");
-verFila(fila3);
 
 
-for(i = 0; i<45; i++){
-  if(rand()%4  == 1){
-      if(fila1 == NULL){
-        i--;
-      }else{
-    filaimpresion = altaFila(filaimpresion, fila1->dato);
-    fila1 = bajaFila(fila1);
-  }
-  }
 
 
-if(rand()%4 == 2){
-  if(fila2 == NULL){
-    i--;
-  }
-
-else{
-  filaimpresion = altaFila(filaimpresion, fila2->dato);
-  fila2 = bajaFila(fila2);
-}
-}
-
-if(rand()%4 == 3){
-    if(fila3 == NULL){
-  i--;
-  }else{
-    filaimpresion = altaFila(filaimpresion, fila3->dato);
-    fila3 = bajaFila(fila3);
-  }
-  }
-
-
-}
+procesoImpresion(fila1, fila2, fila3, filaimpresion);
 
 printf("\nDespues de 30 segundos:\n");
 printf("Fila 1: ");
@@ -169,7 +175,7 @@ verFila(fila3);
 
 
 printf("\n Fila impresion: " );
-verFila(filaimpresion);
+
 Tamano(filaimpresion);
 
 
